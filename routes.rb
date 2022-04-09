ccd5cda99bcc334ec4d1c621a708a0a8dee15d83
shiva <50239824+shivaconceptsolution@users.noreply.github.com>
2022-04-09 09:31:11 -0700

    Add files via upload

--- routes.rb
+++ routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root "articles#index"
   get 'students/index'
   get 'students/new'
+  get 'registration/register'
+  post "regs",to:'registration#registerlogics'
+  get 'registration/login'
+  post 'registration/login'
   resources :articles do
     resources :comments
   end
